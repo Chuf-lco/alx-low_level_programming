c60cd1c61070e15a8f0506702768b545184af9bb
Antony Maina <mainaantony07@gmail.com>
2023-02-16 12:10:59 -0800

    Genius to understand simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char char ="'and thatpiece of art is useful"\" - Dora Korpa, 2015-10-19"
+	putchar(char);
+	return (1);
+}
