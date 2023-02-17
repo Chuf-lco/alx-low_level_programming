f0029971965be4c3756f793ccc8eea4001891f69
Antony Maina <mainaantony07@gmail.com>
2023-02-17 12:00:25 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * description: Print all hexadecimal numbers
+ * Return: Return 0 Success
+ */
+
+int main(void)
+{
+	char n;
+
+	char x;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n);
+	}
+
+	for (m = 'a'; m <= 'f'; m++)
+	{
+		putchar((m % 6 + '0');
+	}
+	putchar('\n');
+	return (0);
+}
