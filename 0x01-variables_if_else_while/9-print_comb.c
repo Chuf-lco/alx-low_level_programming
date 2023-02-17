aaadb697362b83bdd555f5f481f6fae8ff2156f7
Antony Maina <mainaantony07@gmail.com>
2023-02-17 12:15:46 -0800

    patience

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ * Description: Print combination of numbers
+ * Return: Return 0 Success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; n < 10; n++)
+	{
+		putchar(i);
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
