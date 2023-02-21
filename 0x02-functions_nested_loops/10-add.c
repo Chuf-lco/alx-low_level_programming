f7b8ccc3491d6f543784ee810dae3bef10d07ae7
Antony Maina <mainaantony07@gmail.com>
2023-02-21 14:09:42 -0800

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add -Entry point
+ * description: Add integers
+ * @a: Integer
+ * @b: Integer
+ * Return: Return 0 Success
+ */
+
+int add(int b, int a)
+{
+	return (a + b);
+}
