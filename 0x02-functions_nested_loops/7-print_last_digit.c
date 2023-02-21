a9ebc6bf3c1f64cf7f738ae82629635a2acbcdd5
Antony Maina <mainaantony07@gmail.com>
2023-02-21 12:54:39 -0800

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit -Entry point
+ * @i: Print last digit
+ * Return: Return (j) Success
+ */
+
+int print_last_digit(int i)
+{
+	int j;
+
+	j = i % 10;
+
+	return (j);
+}
