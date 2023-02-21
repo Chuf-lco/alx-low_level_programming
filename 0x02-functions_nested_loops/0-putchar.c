64129a5fbecd0d7d248f2443016070bb104ee24c
Antony Maina <mainaantony07@gmail.com>
2023-02-21 04:12:21 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * main -Entry point
+ * description: Print _putchar
+ * Return: Return 0 Success
+ */
+
+int main(void)
+{
+	write(1, "_putchar\n", 9);
+
+	return (0);
+}
