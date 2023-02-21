7d8a722ce0e6bad9fa12ddbf0970b656c780e5aa
Antony Maina <mainaantony07@gmail.com>
2023-02-21 10:15:47 -0800

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower -Entry point
+ * @c: Parameter for lowercase
+ * Description: Checks lowercase character
+ * Return: Return 0 Success
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+
+}
