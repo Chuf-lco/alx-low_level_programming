93a6068eb127aa0fa113db7c90b8464d55001faf
Antony Maina <mainaantony07@gmail.com>
2023-02-21 12:28:24 -0800

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs -Entry point
+ * description: Absolute value
+ * @int i: Absolute value of an integer
+ * Return: Return 0 Success
+ */
+
+int _abs(int i)
+{
+	if (i < 0)
+	{
+		i = -(i);
+	}
+	else
+	{
+		i = i;
+	}
+	return (0);
+
+}
