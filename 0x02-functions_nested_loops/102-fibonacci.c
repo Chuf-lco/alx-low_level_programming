0acd824964da89bcae27d781011f8e7e42c1f273
Antony Maina <mainaantony07@gmail.com>
2023-02-22 02:51:33 -0800

    fibonacci 50

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main -Entry point
+* description: Print fibonacci numbers
+* Return: Return 0 Success
+*/
+
+void main(void)
+{
+	long int i, j, sum;
+	int k;
+
+	printf("1, 2");
+	for (k = 3; k <= 50; k++)
+	{
+		sum = i + j;
+		i = j;
+		j = sum;
+		printf(", %lu", i);
+	}
+	printf("\n");
+}
