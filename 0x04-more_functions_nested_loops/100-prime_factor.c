0d6078dc5ed57312f0d4dc353bd3dacf71c4414b
Antony Maina <mainaantony07@gmail.com>
2023-02-23 13:41:53 -0800

    prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ * description: prime factor
+ * Return: Return 0 Success
+ */
+
+int main(void)
+{
+	long int i, j;
+
+	i = 612852475143;
+
+	for (j = 2; j <= i; i++)
+	{
+		if (i % j == 0)
+		{
+			i /= j;
+			j--;
+		}
+	}
+	printf("%lu", j);
+	return (0);
+}
