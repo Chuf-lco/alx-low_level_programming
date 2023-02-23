1f56cc34004dbfe5f7a678d71bd0b16072fbe44c
Antony Maina <mainaantony07@gmail.com>
2023-02-23 10:53:16 -0800

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul -Entry point
+ * description: Multiply two integers
+ * @a: Integer 1
+ * @b: Integer 2
+ * Return: Return 0 Success
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
