49d90122de1bdbda11c44963be7c9e19de5e2d51
Antony Maina <mainaantony07@gmail.com>
2023-02-23 10:45:28 -0800

    isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -4,14 +4,18 @@
  * _isdigit -Entry point
  * description: Checks for a digit
  * @c: Parameter for digit
- * Return: Return 0 Success
+ * Return: Return 1 Success
  */
 
 int _isdigit(int c)
 {
-	if (c >= 0 && c <= 9)
+	if (c >= 48 && c <= 57)
 	{
 		return (1);
+	}
+	else
+	{
+		return (0);
 	}
 	return (c);
 }
