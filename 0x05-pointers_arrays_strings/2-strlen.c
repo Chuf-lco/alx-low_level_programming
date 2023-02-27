5152c99e99b5bce72d901b96b437de7c0cd1437f
Antony Maina <mainaantony07@gmail.com>
2023-02-27 09:02:00 -0800

    length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen -Entry point
+ * description: String length
+ * @s: String character
+ * Return: Return 0 Success
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = s[];
+	return (&i);
+}
