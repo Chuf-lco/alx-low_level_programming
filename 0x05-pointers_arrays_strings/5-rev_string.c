a313ad47f14121fc895b5bd92bcf73bd4b6eb1b6
Antony Maina <mainaantony07@gmail.com>
2023-02-27 10:59:46 -0800

    Reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * rev_string -Entry point
+ * description: Reverse a string
+ * @s: String character
+ */
+
+void rev_string(char *s)
+{
+	int i = 0;
+
+	while s[i] != '\0'
+	{
+		i++;
+	}
+	for (i = (i - 1); i >=0; i--)
+	{
+		return (s[i]);
+	}
+}
