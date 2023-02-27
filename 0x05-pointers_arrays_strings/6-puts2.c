dc4a220e956cb243eb0c6359d670380c3cc79542
Antony Maina <mainaantony07@gmail.com>
2023-02-27 11:52:14 -0800

    print characters

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,11 +10,15 @@ void puts2(char *str)
 {
 	int i;
 
-	for (; str[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		if ((i % 2) == 0)
 		{
 			_putchar(str[i]);
+		}
+		else
+		{
+			continue;
 		}
 	}
 	_putchar('\n');
