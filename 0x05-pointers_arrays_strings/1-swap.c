f9fac7db7f7413c45be179c01913dd3a88755cc1
Antony Maina <mainaantony07@gmail.com>
2023-02-27 08:42:32 -0800

    swap integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int -Entry point
+ * description: Swap values of two integers
+ * @a: Integer value
+ * @b: Integer value
+ * Return: Return 0 Success
+ */
+
+void swap_int(int *a, int *b)
+{
+	&b = *a;
+}
