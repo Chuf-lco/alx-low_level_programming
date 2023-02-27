84a4d746b1f8632017993d2570ca34dc72146c8f
Antony Maina <mainaantony07@gmail.com>
2023-02-27 12:00:15 -0800

    print half the string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts_half -Entry point
+ * description: Print half of a string
+ * @str: String to be printed
+ */
+
+void puts_half(char *str)
+{
+	int i;
+	int n = (str[i] - 1) / 2;
+
+	for (i = 0; i <= str[i];  i++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
