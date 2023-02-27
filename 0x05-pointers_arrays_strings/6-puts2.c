5f36cff306d4640c6062de26fd23846e9ae5b0aa
Antony Maina <mainaantony07@gmail.com>
2023-02-27 11:42:40 -0800

    print characters

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 -Entry point
+ * description: Print characters of a string
+ * @str: Character variable
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	for(i = 0; i < str[i]; i += 2)
+	{
+		_putchar(str[i];
+	}
+	_putchar('\n');
+}
