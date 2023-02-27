b788e6a2033d613d8dead59cf46ebab4925706a6
Antony Maina <mainaantony07@gmail.com>
2023-02-27 12:50:14 -0800

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcpy -Entry point
+ * description: Copy string
+ * @dest: Pointer
+ * @src: Pointer
+ * Return: Return String Success
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (*(src + i) != '\0')
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+
+	*(dest + i) = '\0';
+}
