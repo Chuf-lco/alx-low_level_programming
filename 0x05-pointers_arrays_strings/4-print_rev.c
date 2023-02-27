ced226607122bc40d0149e90b926d8fe3a3ee8ff
Antony Maina <mainaantony07@gmail.com>
2023-02-27 09:48:57 -0800

    Print reverse string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,13 +8,15 @@
 
 void print_rev(char *s)
 {
-	int i = (&s);
+	int i = (0);
 
-	while (i != '\0')
+	while (s[i] != '\0')
 	{
-		i--;
-		_putchar(i);
-
+		i++;
+	}
+	for (i = i - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
 	}
 	_putchar('\n');
 }
