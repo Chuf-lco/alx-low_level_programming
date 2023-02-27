3b2556a2c7829d2bac8b5975c0fac8f4a8c09a55
Antony Maina <mainaantony07@gmail.com>
2023-02-27 11:18:14 -0800

    reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,14 +8,19 @@
 
 void rev_string(char *s)
 {
-	int i = 0;
+	char a = s[0];/*string*/
+	int i = 0;/*length*/
+	int j; /*temporary hold of the string*/
 
 	while (s[i] != '\0')
 	{
 		i++;
 	}
-	for (i = (i - 1); i >=0; i--)
+	for (j = 0; j < c; i++)
 	{
-		return (s[i]);
+		i--;
+		a = s[j];
+		s[j] = s[i];
+		s[i] = a;
 	}
 }
