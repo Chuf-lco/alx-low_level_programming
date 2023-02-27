b44187371d44ceca161d9788aa5bf7c85651cfd7
Antony Maina <mainaantony07@gmail.com>
2023-02-27 09:29:25 -0800

    Print a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts -Entry point
+ * description: Print a string
+ * @str: Parameter string
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+	_puchar('\n');
+}
