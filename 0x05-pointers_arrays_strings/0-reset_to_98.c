3f40b1ca9f685fe933352afccd15e58f18c3a41d
Antony Maina <mainaantony07@gmail.com>
2023-02-27 08:19:40 -0800

    98 battery

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * reset_to_98 -Entry point
+ * description: Point to 98
+ * @n: Parameter
+ * Return: Retirn 0 Success
+ */
+
+void reset_to_98(int *n)
+{
+	n[0] = 98;
+	putchar(n);
+	putchar('\n');
+}
+
