d33488888797c5e0a2012308a58dede801bdb4b9
Antony Maina <mainaantony07@gmail.com>
2023-03-01 05:31:51 -0800

    Concatenate two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcat -Entry point
+ * description: Concatenate strings
+ * @dest: Destination string
+ * @src: Source string
+ * Return: Return String Success
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *i = dest;
+
+	while (*i != '\0')
+	{
+		i++;
+	}
+
+	while (*src != '\0')
+	{
+		*i++ = *src++;
+	}
+	_putchar('\n');
+	return (dest);
+}
