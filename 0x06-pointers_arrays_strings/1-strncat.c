1db7c01341e907fe4066dd471eda679f0bfedc5b
Antony Maina <mainaantony07@gmail.com>
2023-03-01 09:38:17 -0800

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncat -Entry point
+ * Description: Concatenate two strings
+ * @dest: Destination string
+ * @src: Source string
+ * @n: Number of characters
+ * Return: Return String Success
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char *i = dest;
+
+	while (i != '\0')
+	{
+		i++;
+	}
+
+	while (n--)
+	{
+		if ((*i++ != *src++))
+		{
+			return (dest);
+		}
+	}
+	return (dest);
+}
