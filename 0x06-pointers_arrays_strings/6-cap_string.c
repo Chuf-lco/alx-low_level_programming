2cad0878b10ed581a0eafc052d5574607d2a9182
Antony Maina <mainaantony07@gmail.com>
2023-03-02 12:06:29 -0800

    Word separator

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,28 +14,39 @@ char *cap_string(char *str)
 	i = 0;
 	while (str[i])
 	{
-		while (!(str[i] >= 'a' && str[i] <= 'z'))
-		{
-			i++;
-		}
-		if (str[i - 1] == ' ' ||
-		    str[i - 1] == '\t' ||
-		    str[i - 1] == '\n' ||
-		    str[i - 1] == ',' ||
-		    str[i - 1] == ';' ||
-		    str[i - 1] == '.' ||
-		    str[i - 1] == '!' ||
-		    str[i - 1] == '?' ||
-		    str[i - 1] == '"' ||
-		    str[i - 1] == '(' ||
-		    str[i - 1] == ')' ||
-		    str[i - 1] == '{' ||
-		    str[i - 1] == '}' ||
-		    i == 0)
+		if (str[i] >= 'a' && str[i] <= 'z')
 		{
 			str[i] -= 32;
+		}
+		if (word_separator(str[i]) && (str[i + 1] >= 'a' && str[i + 1] <= 'z'))
+		{
+			str[i + 1] -= 32;
 		}
 		i++
 	}
 	return (str);
+}
+
+/**
+ * word_separator -Entry point
+ * description: Word separators
+ * @c: an input character
+ * Return: 1 if seperator, 0 otherwise
+ */
+
+
+int word_separator(char c)
+{
+	int i = 0;
+
+	char separator[13] = { ' ', '/t', '\n', ',', ';', '.', '!', '?',
+		'"', '(', ')', '{', '}' };
+	for (; i < 13; i++)
+	{
+		if (c == separator[i])
+		{
+			return (1);
+		}
+	}
+	return (0);
 }
