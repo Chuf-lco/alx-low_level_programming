d297fe0572bb197e980db3eed0f88c72c0855171
Antony Maina <mainaantony07@gmail.com>
2023-03-02 08:31:29 -0800

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,17 +11,17 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
-
+	int i;
+	
+	i = 0;
 	while (i < n && src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	
 	while (i < n)
 	{
-		*dest = '\0';
+		dest[i] = '\0';
 		i++;
 	}
 
