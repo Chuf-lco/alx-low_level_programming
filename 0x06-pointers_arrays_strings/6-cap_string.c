d9f6fcb58e41583b94899fdfba27f2df72691862
Antony Maina <mainaantony07@gmail.com>
2023-03-02 09:34:44 -0800

    Capitalize words

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * cap_string -Entry point
+ * description: Capitalize words
+ * @n: array
+ * Return: Return Uppercase
+ */
+
+char *cap_string(char *n)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
+		{
+			i++;
+		}
+		if (str[i - 1] == ' ' ||
+				str[i - 1] == '\t' ||
+				str[i - 1] == '\n' ||
+				str[i - 1] == ',' ||
+				str[i - 1] == ';' ||
+				str[i - 1] == '.' ||
+				str[i - 1] == '!' ||
+				str[i - 1] == '?' ||
+				str[i - 1] == '"' ||
+				str[i - 1] == '(' ||
+				str[i - 1] == ')' ||
+				str[i - 1] == '{' ||
+				str[i - 1] == '}' ||
+				i == 0)
+		{
+			str[i] -= 32;
+		}
+		i++
+	}
+	return (n);
+}
