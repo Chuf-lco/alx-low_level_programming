21321102388427a8710b86aa650901b40d0ed024
Antony Maina <mainaantony07@gmail.com>
2023-03-02 13:00:10 -0800

    encode string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet -Entry point
+ * description: Encode string
+ * @n: Character
+ * Return: Return Character Success
+ */
+
+char *leet(char *n)
+{
+	int i;
+	int j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (n[i] == s1[j])
+			{
+				n[i] = s2[j];
+			}
+		}
+	}
+	return (n);
+}
