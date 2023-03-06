906d297b355b8a87ac1d281be13bf715ddc22709
Antony Maina <mainaantony07@gmail.com>
2023-03-06 13:57:57 -0800

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,7 +12,7 @@ char *_strstr(char *haystack, char *needle)
 {
 	for (; *haystack != '\0'; haystack++)
 	{
-		while (*haystack == *needle && *needle != '\0')
+		while ((*haystack == *needle) && *(needle != '\0'))
 		{
 			haystack++;
 			needle++;
