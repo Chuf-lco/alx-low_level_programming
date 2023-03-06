26941845f393fe5aafa657f40ba47755995c20a5
Antony Maina <mainaantony07@gmail.com>
2023-03-06 12:02:29 -0800

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strspn -Entry point
+ * description: Length of a prefix subtring
+ * @s: String
+ * @accept: Filter string
+ * Return: Return length success
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i = 0;
+	int j;
+
+	while (*s)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (*s == accept[j])
+			{
+				i++;
+			}
+			else if (accept[j + 1] == '\0')
+			{
+				return (i);
+			}
+		}
+		s++;
+	}
+	return (i);
+}
