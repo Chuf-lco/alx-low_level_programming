02d635f845d3b8f449929351ea741ead3a7dcc0e
Antony Maina <mainaantony07@gmail.com>
2023-03-08 13:57:07 -0800

    prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * is_prime_number -Entry point
+ * description: Prime numbers
+ * @n: Parameter
+ * Return: Return prime number Success
+ */
+
+int is_prime_number(int n)
+{
+	if (n % 2 != 0)
+		return (1);
+	return (0);
+}
