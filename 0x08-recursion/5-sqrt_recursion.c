cb4b8de9f5d9daa2fed5d4f771407140ce30eba8
Antony Maina <mainaantony07@gmail.com>
2023-03-08 13:29:17 -0800

    square root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion -Entry point
+ * description: Natural squareroot of a number
+ * @n: Number
+ * Return: Return squareroot Success
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (n / _sqrt_recursion(n, 0));
+}
