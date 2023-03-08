48485341013e04a1a0057356ca1160bce0f896c5
Antony Maina <mainaantony07@gmail.com>
2023-03-08 13:48:15 -0800

    square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,5 +11,23 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	return (n / _sqrt_recursion(n));
+	if (n == 1)
+		return (1);
+	return (n / _abs_sqrt(n, 0));
+}
+/**
+ * _abs_sqrt -Entry point
+ * descrption: find square root
+ * @n: parameter
+ * @i: parameter
+ * Return: Return squareroot Success
+ */
+
+int _abs_sqrt(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (_abs_sqrt(n, i + 1));
 }
