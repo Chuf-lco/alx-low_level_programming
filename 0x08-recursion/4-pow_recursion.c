3c7f0c841f6ab122c03f61fdc6568263b677fed7
Antony Maina <mainaantony07@gmail.com>
2023-03-08 13:12:01 -0800

    raised to power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _pow_recursion -Entry point
+ * description: Raised to the power
+ * @x: parameter for value
+ * @y: parameter for power
+ * Return: Return result Success
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	return (x * _pow_recursion(y));
+}
