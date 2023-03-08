cf0073576c5f1977a0bf7957a69b0dc595bded46
Antony Maina <mainaantony07@gmail.com>
2023-03-08 09:33:44 -0800

    reverse string

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion -Entry point
+ * description: print string in reverse
+ * @s: Pointer parameter
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(s);
+		_print_rev_recursion(s + 1);
+		break;
+	}
+}
