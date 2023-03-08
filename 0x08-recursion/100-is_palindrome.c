acf6c5d0198c64b9d76da47fc4961f6ed9029b48
Antony Maina <mainaantony07@gmail.com>
2023-03-08 14:22:26 -0800

    Palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * is_palindrome -Entry point
+ * description: If a string is a palindrome
+ * @s: String
+ * Return: Return palindrome Success
+ */
+
+int is_palindrome(char *s)
+{
+	if (*s && *s == "")
+		return (1);
+	else
+		return (0);
+
+}
