366bb149559216d5b2e5e71c9573b187a12f4b20
Antony Maina <mainaantony07@gmail.com>
2023-03-10 09:22:56 -0800

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ * @argc: Count
+ * @argv: Array
+ * Return: Return 0 Success
+ */
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
