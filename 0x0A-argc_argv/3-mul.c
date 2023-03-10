20b1a71acc0646d5e8e6ad5e86d09f7ea0d94aa7
Antony Maina <mainaantony07@gmail.com>
2023-03-10 11:03:10 -0800

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int  atoi(char *s);
+/**
+ * main -Entry point
+ * @argc: value
+ * @argv: array
+ * Return: Return 0 Success
+ */
+int main(int argc, char *argv[])
+{
+	int a, m = 1;
+
+	if (argc < 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	for (a = 1; a < argc; a++)
+	{
+		m = m * atoi(argv[a]);
+	}
+	printf("Result = %d\n", m);
+	return (0);
+}
