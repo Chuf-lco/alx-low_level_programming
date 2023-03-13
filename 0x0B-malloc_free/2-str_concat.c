942278028ed9e578817e5256582150c50a102158
Antony Maina <mainaantony07@gmail.com>
2023-03-13 16:16:59 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,54 +1,31 @@
 #include "main.h"
-#include <stdlib.h>
+#include "stdlib.h"
 /**
  * str_concat -Entry point
- * @s1: String 1
- * @s2: String 2
- * Return: Return String Success
+ * @s1: String
+ * @s2: String
+ * Return: Return string Success
  */
-
 char *str_concat(char *s1, char *s2)
 {
-	int len1, len2, i, j;
+	int i, j, len1 = 0, len2 = 0;
 	char *final_str;
 
-	if (s1 == NULL)
-	{
+	if (!s1)
 		s1 = "";
-	}
-	if (s2 == NULL)
-	{
+	if (!s2)
 		s2 = "";
-	}
-	len1 = 0;
-	while (s1[len1] != '\0')
-	{
+	while (s1[len1])
 		len1++;
-	}
-	len2 = 0;
-	while (s2[len2] != '\0')
-	{
+	while (s2[len2])
 		len2++;
-	}
-	final_str = (char *) malloc(len1 + len2 + 1);
-	if (final_str == NULL)
-	{
+	final_str = malloc(len1 + len2 + 1);
+	if (!final_str)
 		return (NULL);
-	}
-	i = 0;
-	while (s1[i] != '\0')
-	{
-		final_str[i] = s1[i];
-		i++;
-	}
-	j = 0;
-	while (s2[j] != '\0')
-	{
-		final_str[j] = s2[j];
-		i++;
-		j++;
-	}
-	final_str[i] = '\0';
+	for (i = 0; i < len1; i++)
+		final_str[i] = s[i];
+	for (i = 0; i < len2; i++)
+		final_str[i + len1] = s2[i];
+	final_str[len1 + len2] = '\0';
 	return (final_str);
-
 }
