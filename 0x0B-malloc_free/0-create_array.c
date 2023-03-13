09ac3e8d00b2badf08211e85112800e4d0f6461e
Antony Maina <mainaantony07@gmail.com>
2023-03-13 14:20:59 -0700

    task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * create_array -Entry point
+ * @size :length of array
+ * @c: Parameter
+ * Return: Return array Success
+ */
+
+
+char *create_array(unsigned int size, char c)
+{
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	char *arr = (char  *) malloc(size * sizeof(char));
+
+	if (arr == 0)
+	{
+		return (NULL);
+	}
+
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+	{
+		array[i] = c;
+	}
+	return (array);
+}
