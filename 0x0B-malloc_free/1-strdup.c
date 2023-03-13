6249bcc10ee465b65b75bebb888a6d7851e9ebbc
Antony Maina <mainaantony07@gmail.com>
2023-03-13 14:56:20 -0700

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _strdup -Entry point
+ * @str: String
+ * Return: Return String Success
+ */
+char *_strdup(char *str)
+{
+	char newstr;
+	int i;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	newstr == str;
+	newstr = char * +malloc(sizeof(str));
+
+	if (newstr[i] < newstr)
+	{
+		return (NULL);
+	}
+
+	return (newstr);
+}
