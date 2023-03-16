e3219dcb37eef9cd0af9cf3dcac2ec73f7be7818
Antony Maina <mainaantony07@gmail.com>
2023-03-16 11:53:30 -0700

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,7 +18,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 	while (s1[len1] != '\0')
 		len1++;
-	while (s2[len2] != '\0' && len2 <= n)
+	while (s2[len2] != '\0' && len2 < n)
 		len2++;
 	len3 = len1 + len2 + 1;
 	concat_str = malloc(len3);
@@ -29,7 +29,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		concat_str[i] = s1[i];
 		i++;
 	}
-	while (s2[j] != '\0' && j <= n)
+	while (s2[j] != '\0' && j < n)
 	{
 		concat_str[i] = s2[j];
 		i++;
