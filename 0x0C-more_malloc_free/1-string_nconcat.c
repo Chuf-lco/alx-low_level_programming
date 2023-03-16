7335ce47a5138fbf7898ba6d0d9c1fe324ec88a6
Antony Maina <mainaantony07@gmail.com>
2023-03-16 11:55:46 -0700

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,9 +17,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 	while (s1[len1] != '\0')
+	{
 		len1++;
+	}
 	while (s2[len2] != '\0' && len2 < n)
+	{
 		len2++;
+	}
 	len3 = len1 + len2 + 1;
 	concat_str = malloc(len3);
 	if (!concat_str)
