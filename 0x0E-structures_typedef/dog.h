ddaa5ff4de4b6a2bba692175700aedf464275f7c
Antony Maina <mainaantony07@gmail.com>
2023-03-20 16:11:33 -0700

    dog_t struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,6 +15,19 @@ struct dog
 	float age;
 	char *owner;
 };
+
+struct dog *dog_t(char *name. float age, char *owner)
+{
+	struct dog *dog;
+
+	dog = malloc(sizeof(struct dog));
+	if (dog ==NULL)
+		return (NULL);
+	dog->name = name;
+	dog->age = age;
+	dog->owner = owner;
+	return dog;
+}
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
