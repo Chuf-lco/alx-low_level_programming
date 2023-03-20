7ad72ac6f0df7d088f6277726352af8c72eed792
Antony Maina <mainaantony07@gmail.com>
2023-03-20 13:41:14 -0700

    file name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -Program name
+ * @argc: Parameter
+ * @argv: Pointer
+ * Return: Return 0 Success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
