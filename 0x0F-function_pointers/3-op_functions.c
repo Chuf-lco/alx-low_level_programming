06abe2982f2e6b6506e8094f8f525bba8047e53e
Antony Maina <mainaantony07@gmail.com>
2023-03-22 15:45:02 -0700

    Simple operations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include "3-calc.h"
+/**
+ *  * op_add -Function for addition
+ *   * @a: Integer
+ *    * @b: Integer
+ *     * Return: Return addition result
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ *  * op_sub -Function for subtraction
+ *   * @a: Integer
+ *    * @b: Integer
+ *     * Return: Return subtraction result
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ *  * op_mul -Function for multiplication
+ *   * @a: Integer
+ *    * @b: Integer
+ *     * Return: Return multiplication result
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ *  * op_div -Function for division
+ *   * @a: Integer
+ *    * @b: Integer
+ *     * Return: Return division result
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod -Function for modulus
+ * @a: Integer
+ * @b: Integer
+ * Return: Return modulus result
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
