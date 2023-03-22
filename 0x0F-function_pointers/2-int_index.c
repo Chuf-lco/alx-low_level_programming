10ef96409efff06e182388459ab92b71926496d7
Antony Maina <mainaantony07@gmail.com>
2023-03-22 15:57:53 -0700

    Search for an integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -9,14 +9,14 @@
  * Return: Return integer Success
  */
 int int_index(int *array, int size, int (*cmp)(int))
-{i
+{
 	int i;
 
 	if (array == NULL || cmp == NULL)
 	{
 		return (-1);
 	}
-	for (i = 0 ; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) != 0)
 		{
