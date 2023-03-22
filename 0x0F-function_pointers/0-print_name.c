cdaa011fff5d13cc5cfd81fd419b31cabd884f3b
Antony Maina <mainaantony07@gmail.com>
2023-03-22 13:09:43 -0700

    Execute function in array

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-#include "_putchar.c"
+#include <stdio.h>
 /**
  * print_name -Print a name
  * @name: Arguement
---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+#include  <stdio.h>
+
+/**
+ * array_iterator -Execute function parameter in array
+ * @array: Pointer
+ * @size: Size of array
+ * @action :Function pointer
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
