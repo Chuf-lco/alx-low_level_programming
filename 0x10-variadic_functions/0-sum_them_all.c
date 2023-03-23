e939a854e7613da1e36fd9d8c599500052e36a18
Antony Maina <mainaantony07@gmail.com>
2023-03-23 16:44:57 -0700

    Sum of parameters

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,17 +10,14 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list args;
-	unsigned int i, sum;
+	unsigned int i, sum = 0;
 
-	va_start (args, n);/*Initialize argument list*/
+	va_start(args, n);/*Initialize argument list*/
 
-	if (n == 0)
-		return (0);
-	sum = 0;
 	for (i = 0; i < n; i++)
 	{
-		sum += va_arg (args, int);/*Get next argument value*/
+		sum += va_arg(args, int);/*Get next argument value*/
 	}
-	va_end (args);/*Clean up*/
+	va_end(args);/*Clean up*/
 	return (sum);
 }
