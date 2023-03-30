622dc5d3a0d6eba8d1e7f67d70484a554f5d71d4
Chuf-lco <mainaantony07@gmail.com>
2023-03-30 15:19:58 -0700

    New node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <string.h>
+/**
+ *add_node -Entry point for a new node
+* @head: pointer to first node
+* @str: Pointer to string
+* Return: Return Address of new node Success
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *h;/*declaring new node */
+
+	h = malloc(sizeof(list_t));/*allocate memory to h*/
+	if (h == NULL)
+	{
+		return (NULL);
+	}
+	h->str = strdup(str);/*Duplicating and storing the string*/
+	if (h->str == NULL)
+	{
+		free(h);
+		return (NULL);
+	}
+/*set string length and next pointer*/
+	h->len = strlen(str);
+	h->next = *head;
+	/*update head to point to h*/
+	*head = h;
+	return (h);
+}
