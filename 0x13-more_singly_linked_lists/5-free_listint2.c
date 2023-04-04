7a5fd5a3899afd39c046ccd1e31ca77ec277078d
Chuf-lco <mainaantony07@gmail.com>
2023-04-04 14:21:51 -0700

    Free list 2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 -Free a list
+ * @head: Double pointer for first node
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *i, *next;
+/*@i iterates through list @next points to next node */
+
+	if (head == NULL)
+	{
+		return;
+	}
+
+	while (i != NULL)
+	{
+		next = i->next;
+		free(i);
+		i = next;
+	}
+	head = NULL;
+}
