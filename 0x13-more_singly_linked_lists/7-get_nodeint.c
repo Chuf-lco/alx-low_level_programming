3cfa2cca894a4c283c5c84d77bd396e51092cc7d
Chuf-lco <mainaantony07@gmail.com>
2023-04-04 15:37:02 -0700

    Return nth node

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index -Get nth node
+ * @head: Pointer to list
+ * @index: Index of the node
+ * Return: Return node Success
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *node;
+	unsigned int i;
+
+	node = head;/*points to present node*/
+	if (node == NULL)
+	{
+		return (NULL);
+	}
+	/*iterate through nodes*/
+	for  (i = 0; i < index; i++)
+	{
+		node = node->next;
+	}
+
+	return (node);
+}
