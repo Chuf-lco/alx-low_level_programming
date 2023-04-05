aba17cf0bb15d9f173ea3c62e33deb36e2517e17
Chuf-lco <mainaantony07@gmail.com>
2023-04-05 03:51:38 -0700

    Delete node at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -24,7 +24,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 	h = *head;/*h pointer to previous node*/
 	t = (*head)->next;/*t pointer to current node*/
-	if (*h == NULL)
+	if (h == NULL)
 	{
 		return (-1);
 	}
