20049d8cdf4ed4d9123dcd895adedac8f64ec506
Chuf-lco <mainaantony07@gmail.com>
2023-04-09 05:38:12 -0700

    binary Representation of a number

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_binary - Prints binary representation
+ * @n: Number represented
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int m;
+/*Initialize mask with enough bit set*/
+	m = 1UL << (sizeof(int) * 3 - 2);
+	while (m > 0)
+	{
+		if (n & m)/*checks if bit value is set*/
+		{
+			printf("1");
+		}
+		else
+		{
+			printf("0");
+		}
+		m >>= 1;
+	}
+}
